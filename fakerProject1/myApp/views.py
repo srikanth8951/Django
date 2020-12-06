8ac560223f55faaf088c64210466c330fb4c0d01
srikanth8951 <hvsrikanth98@gmail.com>
2020-12-06 19:32:15 +0530

    commit five

--- fakerProject1/myApp/views.py
+++ fakerProject1/myApp/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from myApp.models import Student
 # Create your views here.
 def view1(request):
+    #s=Student.objects.filter(marks__gt=40) greater than #QuerrySet
+    #s=Student.objects.filter(marks__lt=40) less than
+    #s=Student.objects.filter(marks__lte=18) less than or equal
+    #s=Student.objects.filter(marks__gte=20) greater than or equal
+    #s=Student.objects.filter(marks__exact=20)
+    #s=Student.objects.filter(name__exact="Logan Miller")
+    #s=Student.objects.filter(name__iexact="logan Miller")
+    #s=Student.objects.filter(name__exact="Logan Miller")
+    #s=Student..objects.exclude(marks_range=(40,50))
     s=Student.objects.all()
     d={'stud':s}
     return render(request,'myApp/1.html',d)
