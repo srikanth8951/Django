abc911358fc1d084af41a8c426e08f204f3c7f67
srikanth8951 <hvsrikanth98@gmail.com>
2020-12-08 22:07:11 +0530

    Faker Project part 2

---
+++ fbvProject/myApp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from myApp.models import Employee
+# Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    l=['eno','ename','esal','eaddr']
+
+admin.site.register(Employee,EmployeeAdmin)
