0f912f0dc278089d8bc47b8a3669576244e25501
srikanth8951 <hvsrikanth98@gmail.com>
2020-12-22 21:26:31 +0530

    first commit

---
+++ blogProject/myApp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from myApp.models import Post
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    l=['title','slug','author','publish','created','updated','status']
+    prepopulated_fields={'slug':('title',)}
+    list_filter=('status','created','publish','author')
+    search_fields=['title','body']
+    raw_id_fields=('author',)
+    ordering=['status','publish']
+admin.site.register(Post,PostAdmin)
